75b371c4c47a966beddffcbe7bdd0478c539469f
Richard Tillies <rtillies@gmail.com>
2022-03-29 11:40:43 -0400

    Initial commit

---
+++ lib/bank_account.rb
@@ -0,0 +1,23 @@
+class BankAccount
+  attr_reader :account_number,
+              :secret_code,
+              :balance
+
+  def initialize(id, pin, balance)
+    @account_number = id
+    @secret_code = pin
+    @balance = balance
+  end
+
+  def deposit(amount)
+
+  end
+
+  def withdraw(amount)
+
+  end
+
+  def verify?(id, pin)
+
+  end
+end
---
+++ spec/bank_account_spec.rb
@@ -0,0 +1,54 @@
+require 'pry'
+require 'rspec'
+require './lib/bank_account'
+
+describe BankAccount do
+  it '1. exists' do
+    checking = BankAccount.new(123, '0987', 250.0)
+
+    expect(checking).to be_an BankAccount
+  end
+
+  it '2. has attributes' do
+    checking = BankAccount.new(123, '0987', 250.0)
+
+    expect(checking.account_number).to eq 123
+    expect(checking.secret_code).to eq '0987'
+    expect(checking.balance).to eq 250.0
+  end
+
+  xit '3. can deposit money' do
+    checking = BankAccount.new(123, '0987', 250.0)
+    checking.deposit(100.0)
+
+    expect(checking.balance).to eq 350.0
+  end
+
+  xit '4. can withdraw money' do
+    checking = BankAccount.new(123, '0987', 250.0)
+    checking.withdraw(55.0)
+
+    expect(checking.balance).to eq 195.0
+  end
+
+  xit '5. cannot overdraw account' do
+    checking = BankAccount.new(123, '0987', 250.0)
+    checking.withdraw(255.0)
+    expect(checking.balance).to eq 250.0
+
+    savings = BankAccount.new(567, '2468', 500.0)
+    savings.withdraw(200.0)
+    savings.withdraw(200.0)
+    savings.withdraw(200.0)
+    expect(savings.balance).to eq 100.0
+  end
+
+  xit '6. can verify account details' do
+    checking = BankAccount.new(123, '0987', 250.0)
+    savings = BankAccount.new(567, '2468', 500.0)
+    id = 123; pin = '0987'
+
+    expect(checking.verify?(id, pin)).to eq true
+    expect(savings.verify?(id, pin)).to eq false
+  end
+end
