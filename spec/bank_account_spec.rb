e409b228dca474ecc36aea3986401466c3062321
Meg Stang <mstang15@gmail.com>
2022-03-29 09:55:18 -0600

    Unskip deposit test

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -17,7 +17,7 @@ describe BankAccount do
     expect(checking.balance).to eq 250.0
   end
 
-  xit '3. can deposit money' do
+  it '3. can deposit money' do
     checking = BankAccount.new(123, '0987', 250.0)
     checking.deposit(100.0)
 
